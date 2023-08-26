a76c3a1b4e6f79c1fc2636d6c0e3ec374bb6e57c
Quang Huy <quanghuy@example.net>
2023-08-26 21:50:50 +0700

    add selectors

--- js/selectors.js
+++ js/selectors.js
@@ -3,7 +3,19 @@
 // 2. Current Turn
 // 3. Replay Game
 // 4. Game status
-export function getCellElementList() {}
-export function getCurrentTurnElement() {}
-export function getCellElementAtIdx() {}
-export function getGameStatusElement() {}
+// Danh sách các thẻ li
+export function getCellElementList() {
+    return document.querySelectorAll("#cellList > li");
+}
+// Chỗ turn lấy x thành o
+export function getCurrentTurnElement() {
+    return document.getElementById("currentTurn");
+}
+
+export function getCellElementAtIdx(index) {
+    return document.querySelector(`#cellList > li:nth-child(${index + 1})`);
+}
+
+export function getGameStatusElement() {
+    return document.getElementById("gameStatus");
+}
